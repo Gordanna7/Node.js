4c85c01cf66f0b3c1f5c4475386bdf6268b1bcef
Gordana <gordannaa4@gmail.com>
2023-02-15 20:57:44 +0100

    commit c2

---
+++ 01-02/index.js
@@ -0,0 +1,18 @@
+console.log("hi world");
+console.log(5 != 5);
+console.log(typeof 25);
+
+const chovek = {
+  name: "John",
+  surname: "Doe",
+};
+
+console.log(chovek);
+
+student = {
+  ime: "Pero",
+  prezime: "Nakov",
+  age: 25,
+};
+
+console.log(student);
