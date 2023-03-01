c399af7bc3d0c709f62171ead47c08913ec2d3b5
Gordana <gordannaa4@gmail.com>
2023-03-01 21:37:48 +0100

    studenti, sort

--- 04/index.js
+++ 04/index.js
@@ -42,35 +42,35 @@ const path = require("path");
 // fileWrite();
 // console.log("se izvirsi i ova");
 
-// const fileWrite = (filename, data) => {
-//   return new Promise((succes, fail) => {
-//     fs.writeFile(filename, data, (err) => {
-//       if (err) return fail(err);
-//       return succes();
-//     });
-//   });
-// };
+const fileWrite = (filename, data) => {
+  return new Promise((succes, fail) => {
+    fs.writeFile(filename, data, (err) => {
+      if (err) return fail(err);
+      return succes();
+    });
+  });
+};
 
-// fileWrite(path.join(__dirname, "test", "ocenki.txt"), "5, 3 ,53, 53, 53,5")
-//   .then(() => {
-//     // ako then e successed
-//     console.log("Success!");
-//     return fileWrite(
-//       path.join(__dirname, "test", "boi.txt"),
-//       "crvena, zholta, zelena"
-//     );
-//   })
-//   .then(() => {
-//     //then == success
-//     console.log("Success2");
-//     return fileWrite(
-//       path.join(__dirname, "test", "boi2.txt"),
-//       "plava, zholta, violetova"
-//     );
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+fileWrite(path.join(__dirname, "test", "ocenki.txt"), "5, 3 ,53, 53, 53,5")
+  .then(() => {
+    // ako then e successed
+    console.log("Success!");
+    return fileWrite(
+      path.join(__dirname, "test", "boi.txt"),
+      "crvena, zholta, zelena"
+    );
+  })
+  .then(() => {
+    //then == success
+    console.log("Success2");
+    return fileWrite(
+      path.join(__dirname, "test", "boi2.txt"),
+      "plava, zholta, violetova"
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const fileRead = (filename) => {
   return new Promise((succes, fail) => {
@@ -95,6 +95,27 @@ fileRead(path.join(__dirname, "test", "boi.txt"))
   try {
     let ocenki = await fileRead(path.join(__dirname, "test", "ocenki.txt"));
     console.log("ocenki: ", ocenki);
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
+let imenik = [
+  { ime: "Zlate Zlatevski", telefon: 09999999 },
+  { ime: "Marko Markovski", telefon: 09999933299 },
+  { ime: "Pero Perovski", telefon: 09333399 },
+];
+
+(async () => {
+  try {
+    let imenikData = JSON.stringify(imenik); //convert object to string
+    console.log(imenikData);
+    await fileWrite(path.join(__dirname, "test1", "imenik.txt"), imenikData);
+    let dataString = await fileRead(
+      path.join(__dirname, "test1", "imenik.txt")
+    );
+    let data = JSON.parse(dataString); //convert string to object
+    console.log(data);
   } catch (err) {
     console.log(err);
   }
